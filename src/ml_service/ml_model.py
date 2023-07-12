e29b901111c2b58422af5989d7f7c58993430eb2
Kirill Smotrov <kirya.smotrov.02@mail.ru>
2023-07-12 22:04:52 +0300

    refactor ml_service

--- src/ml_service/ml_model.py
+++ src/ml_service/ml_model.py
@@ -4,20 +4,24 @@ import tensorflow.keras as keras
 import os
 import pickle
 
-url = 'https://disk.yandex.ru/client/disk/aclImdb'
-origin_dir = 'C:\\programes\\greenatom\\src\\ml_service'
-train_dir = url + '/train'
-test_dir = origin_dir + '/test'
+
+def add_data(dir_path, arr_1, arr_2):
+    for fname in os.listdir(dir_path + '\\neg'):
+        with open(train_dir + '\\neg\\' + fname, 'r', encoding='utf8') as f:
+            arr_1.append(f.read())
+        arr_2.append(0)
+    for fname in os.listdir(dir_path + '\\pos'):
+        with open(train_dir + '\\pos\\' + fname, 'r', encoding='utf8') as f:
+            arr_1.append(f.read())
+        arr_2.append(1)
+
+
+origin_dir = 'C:\\programes\\greenatom\\src\\ml_service\\aclImdb'
+train_dir = origin_dir + '\\train'
+test_dir = origin_dir + '\\test'
 texts = []
 labels = []
-for fname in os.listdir(train_dir + '\\neg'):
-    with open(train_dir + '\\neg\\' + fname, 'r', encoding='utf8') as f:
-        texts.append(f.read())
-    labels.append(0)
-for fname in os.listdir(train_dir + '\\pos'):
-    with open(train_dir + '\\pos\\' + fname, 'r', encoding='utf8') as f:
-        texts.append(f.read())
-    labels.append(1)
+add_data(train_dir, texts, labels)
 
 maxlen = 1000
 max_words = 10000
@@ -45,10 +49,22 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 
 history = model.fit(data, labels, epochs=10, batch_size=32, validation_split=0.2)
 
-loss, accuracy = model.evaluate(data, labels)
+test_data = []
+test_labels = []
+add_data(test_dir, test_data, test_labels)
+
+sequencesTest = tokenizer.texts_to_sequences(test_data)
+dataTest = keras.preprocessing.sequence.pad_sequences(sequencesTest, maxlen=maxlen)
+indicesTest = np.arange(dataTest.shape[0])
+np.random.shuffle(indicesTest)
+data_test = data[indicesTest]
+labels_test = np.array(test_labels)[indicesTest]
+
+loss, accuracy = model.evaluate(data_test, labels_test)
 
 print("Loss: ", loss)
 print("Accuracy: ", accuracy)
+
 model.save('my_model.h5')
 with open('tokenizer.pickle', 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
